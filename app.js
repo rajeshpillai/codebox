44607d3c1f3e52ba875377671a5f61fb31603836
Radhika <radhika.hariharan@gmail.com>
2020-02-09 23:00:41 +0530

    Added debounce

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
 function compile() {
   var html = document.getElementById("html");
   var css = document.getElementById("css");
   var js = document.getElementById("js");
   var code = document.getElementById("code").contentWindow.document;
 
-  document.body.onkeyup = function() {
-    console.log(js.value);
+  var onKeyup = debounce(function() {
     code.open();
     code.writeln(`
       ${html.value} 
@@ -20,7 +39,11 @@ function compile() {
       </script>
     `);
     code.close();
-  };
+  
+  }, 250);
+
+
+  document.body.onkeyup = onKeyup;
 }
 
 compile();
